c9404b9dabaf9b930f170752ed999e368b5f42f9
John <ashton.basile@gmail.com>
2023-09-14 14:17:23 +0100

    first commit

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Student {
+    public static void printNums(){
+        for (int i = 6; i <= 10; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
